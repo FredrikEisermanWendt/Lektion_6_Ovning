1e4c21740c6ad45aa78b2a47b15751a4318da45f
FredrikEisermanWendt <fredrikeiserman@gmail.com>
2023-10-03 13:04:04 +0200

    Första uppgiften som ska versionhanteras

--- src/com/Fredrik/demo/Main.java
+++ src/com/Fredrik/demo/Main.java
@@ -2,36 +2,10 @@ package com.Fredrik.demo;
 
 public class Main {
     public static void main(String[] args) {
-        int[] intArr = {1, 2, 3, 4};
 
-        try {
-            for (int i = 0; i < intArr.length + 1; i++) {
-                System.out.println(intArr[i]);
-            }
-        }catch(Exception e){
-            System.out.println("Error, for loopen räddad!");
+        for(int i = 0; i < 10; i++){
+            System.out.println(i);
         }
-
-        try {
-            if (intArr[intArr.length + 1] == 0){
-                System.out.println(true);
-            }
-        }catch(Exception e){
-            System.out.println("Där räddades if satsen");
-        }
-
-
-    }
-
-
-    public static void tryMethod(){
-        try{
-            System.out.println(2/0);;
-        } catch (Exception e){
-            System.out.println("Error");
-
-        }
-
 
     }
 }
