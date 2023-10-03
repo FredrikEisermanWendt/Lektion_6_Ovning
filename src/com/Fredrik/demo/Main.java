3cc085b85839d440def9031332e9069d1c90d6a5
FredrikEisermanWendt <fredrikeiserman@gmail.com>
2023-10-03 12:51:26 +0200

    Övning 2

--- src/com/Fredrik/demo/Main.java
+++ src/com/Fredrik/demo/Main.java
@@ -2,6 +2,11 @@ package com.Fredrik.demo;
 
 public class Main {
     public static void main(String[] args) {
+        int[] intArr = {1, 2, 3, 4};
+
+        for (int i = 0; i < intArr.length +1; i++){
+            System.out.println(intArr[i]);
+        }
 
 
     }
