2378167c03d5b6de7b919a69f72edf31f58a58a3
FredrikEisermanWendt <fredrikeiserman@gmail.com>
2023-10-03 09:06:29 +0200

    First Commit

---
+++ src/com/Fredrik/demo/Main.java
@@ -0,0 +1,17 @@
+package com.Fredrik.demo;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+    }
+
+
+    public static void tryMethod(){
+        try{
+            System.out.println();
+        }
+
+
+    }
+}
