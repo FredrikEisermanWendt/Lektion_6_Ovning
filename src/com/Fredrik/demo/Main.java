48cead1a564fc13bf9a4b7ee44f5db5d017da0e5
FredrikEisermanWendt <fredrikeiserman@gmail.com>
2023-10-03 13:16:02 +0200

    Code Analysis

--- src/com/Fredrik/demo/Main.java
+++ src/com/Fredrik/demo/Main.java
@@ -3,8 +3,24 @@ package com.Fredrik.demo;
 public class Main {
     public static void main(String[] args) {
 
-        for(int i = 0; i < 10; i++){
-            System.out.println(i);
+        /* TODO
+         *   Change the names of files + packages to relevant names
+         *   Find the errors in the code and resolve it!
+         */
+
+    Student student = new Student(15);
+    student.sayHello("Kalle");
+
+    runMyForLoop();
+
+    }
+
+
+
+
+    public static void runMyForLoop() {
+        for (int i = 0; i < 5; i++){
+            System.out.println("Inside the method runMyForLoop" + i);
         }
 
     }
---
+++ src/com/Fredrik/demo/Student.java
@@ -0,0 +1,21 @@
+package com.Fredrik.demo;
+
+public class Student {
+
+    int age;
+    String name;
+
+    public Student(int age) {
+        this.age = age;
+    }
+
+    public static int DoubleMyAge() {
+        return 2+2;
+
+
+    }
+    public static void sayHello(String name) {
+        System.out.println("Hello " + name);
+
+    }
+}
